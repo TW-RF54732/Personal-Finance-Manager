2ef88d72f46a0a407463c69557acd096da873909
TW-RF54732 <127068503+TW-RF54732@users.noreply.github.com>
2025-12-05 16:42:13 +0800

    add get log by id and filtered and sorted logs

--- api/DataBaseAPI.py
+++ api/DataBaseAPI.py
@@ -1,9 +1,40 @@
 from fastapi import APIRouter
 from dataBase.FinanceDB import FinanceDB,FinanceService,Direction,SortField
 from datetime import datetime
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 router = APIRouter()
+
+db = FinanceDB('sqlite:///dataBase/DB/finance.db')
+service = FinanceService(db)
+
+class SearchRequest(BaseModel):
+    category_name: Optional[str] = None
+    direction: Optional[Direction] = None
+    min_amount: Optional[float] = None
+    max_amount: Optional[float] = None
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
+    note_keyword: Optional[str] = None
+    sort_by: SortField = SortField.TIMESTAMP
+    reverse: bool = True
+    limit: Optional[int] = None
 
 @router.get("/")
 async def db_index():
     return {"message": 'This is data base api'}
 
+@router.get('/get/all_catogry')
+def get_all_catogry():
+    return service.get_all_categories()
+
+@router.get("/get/log_by_id/{log_id}")
+def get_log_by_id(log_id:int):
+    return service.get_log_by_id(log_id=log_id)
+
+@router.post("/get/filtered_and_sorted_logs")
+def get_filt(req: SearchRequest)->list[dict]:
+    params = req.model_dump()
+    result = service.get_filtered_and_sorted_logs(**params)
+    return result
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from api import api_model_ex
+from api import api_model_ex, DataBaseAPI
 app = FastAPI()
 
-app.include_router(api_model_ex.router,prefix='/test',tags=['Test'])
+app.include_router(api_model_ex.router,prefix='/api/test',tags=['Test'])
+app.include_router(DataBaseAPI.router,prefix='/api/db',tags=['dataBase'])
 
 @app.get('/')
 def index():
