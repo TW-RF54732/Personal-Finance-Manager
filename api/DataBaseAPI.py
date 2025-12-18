b2356f6e5b24d9ae05793df4d4406172d1280d02
TW-RF54732 <leo173842558@gmail.com>
2025-12-18 20:31:54 +0800

    fix: DataBaseAPI old route and bad methods

--- api/DataBaseAPI.py
+++ api/DataBaseAPI.py
@@ -48,21 +48,6 @@ def get_service():
 # --- 3. Router 定義 ---
 router = APIRouter()
 
-db = FinanceDB('sqlite:///dataBase/DB/finance.db')
-service = FinanceService(db)
-
-class SearchRequest(BaseModel):
-    category_name: Optional[str] = None
-    direction: Optional[Direction] = None
-    min_amount: Optional[float] = None
-    max_amount: Optional[float] = None
-    start_date: Optional[datetime] = None
-    end_date: Optional[datetime] = None
-    note_keyword: Optional[str] = None
-    sort_by: SortField = SortField.TIMESTAMP
-    reverse: bool = True
-    limit: Optional[int] = None
-
 @router.get("/")
 async def db_index():
     return {"message": 'Finance Database API is running'}
@@ -208,17 +193,3 @@ async def get_filtered_logs(
         return results
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-@router.get('/get/all_catogry')
-def get_all_catogry():
-    return service.get_all_categories()
-
-@router.get("/get/log_by_id/{log_id}")
-def get_log_by_id(log_id:int):
-    return service.get_log_by_id(log_id=log_id)
-
-@router.post("/get/filtered_and_sorted_logs")
-def get_filt(req: SearchRequest)->list[dict]:
-    params = req.model_dump()
-    result = service.get_filtered_and_sorted_logs(**params)
-    return result
