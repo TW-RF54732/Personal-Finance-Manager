cec220976f381702463a7aee41cf904660830e5c
TW-RF54732 <leo173842558@gmail.com>
2025-12-18 21:38:43 +0800

    test

--- testDB2.py
+++ testDB2.py
@@ -5,4 +5,4 @@ from testTool import print_table
 db = FinanceDB(db_url="sqlite:///DB/test2.db", echo=False)
 service = FinanceService(db)
 print(service.get_filtered_and_sorted_logs())
-# print(sum(service.get_filtered_and_sorted_logs(start_date=datetime(2025,10,1),end_date=datetime(2025,10,31),direction=Direction.Income)))
+#    print(sum(service.get_filtered_and_sorted_logs(start_date=datetime(2025,10,1),end_date=datetime(2025,10,31),direction=Direction.Income)))
