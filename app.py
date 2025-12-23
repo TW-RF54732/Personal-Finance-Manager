7f6c9ea5445d055aa299ad6c48059fcaf1291ee2
TW-RF54732 <leo173842558@gmail.com>
2025-12-24 01:12:42 +0800

    feat: goal api

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from api import api_model_ex
 from api import DataBaseAPI
 from api import analyzer
+from api import goal
+
 from dataBase.FinanceDB import FinanceDB,FinanceService
 from data.config import sql_url
 
@@ -13,9 +15,12 @@ app = FastAPI()
 
 app.dependency_overrides[DataBaseAPI.get_service] = lambda: service
 app.dependency_overrides[analyzer.get_service] = lambda: service
+app.dependency_overrides[goal.get_service] = lambda: service
+
 app.include_router(api_model_ex.router,prefix='/api/test',tags=['Test'])
-app.include_router(DataBaseAPI.router,prefix='/api/database',tags=['Aatabase API'])
+app.include_router(DataBaseAPI.router,prefix='/api/database',tags=['Database API'])
 app.include_router(analyzer.router,prefix='/api/analyze',tags=['Analyze'])
+app.include_router(goal.router, prefix='/api/goal', tags=['Goal'])
 
 @app.get('/')
 def index():
